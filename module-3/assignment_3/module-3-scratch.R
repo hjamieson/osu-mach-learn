77d1aa81a76a073428479fde8651095a9823a898
Hugh Jamieson <jamiesoh@oclc.org>
2018-01-27 16:18:36 -0500

    101 beer kitchen

--- module-3/assignment_3/module-3-scratch.R
+++ module-3/assignment_3/module-3-scratch.R
@@ -10,3 +10,21 @@ cost <- function(from, to){
   c(rowx[minx], colnames(rowx)[minx])
 }
 cost(1,2)
+
+price_matrix[1][[1]]
+length(airports)
+mx <- matrix(0,6,6)
+mx[1,1]
+mx
+
+lowcost <- function(from, to){
+  rowx <- price_matrix[[from]][to,]
+  minx <- 1
+  posx <- 1
+  while (posx <= ncol(rowx)){
+    if (rowx[posx] < rowx[minx]) minx <- posx
+    posx <- posx + 1
+  }
+  rowx[minx,1]
+}
+lowcost(1,2)
